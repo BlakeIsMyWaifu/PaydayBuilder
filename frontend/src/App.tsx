b29383101d5de49f9752e81744677bab3e870fd6
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-04-24 17:37:33 +0100

    rename Masks to Mask

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 import { MemoryRouter, Route, Switch } from 'react-router'
 
 import Home from './pages/Home'
-import Masks from './pages/Mask'
+import Mask from './pages/Mask'
 
 const App: React.FC = () => {
 	return (
 		<MemoryRouter>
 			<Switch>
 				<Route exact path='/' component={Home} />
-				<Route path='/mask' component={Masks} />
+				<Route path='/mask' component={Mask} />
 			</Switch>
 		</MemoryRouter>
 	)
