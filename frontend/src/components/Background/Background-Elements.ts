1f3bbcd55bef31fdcae28c8fe5d763d6ed7b80f0
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-04-24 15:45:22 +0100

    added background component

---
+++ frontend/src/components/Background/Background-Elements.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Image = styled.img`
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+
+	z-index: -1;
+	user-select: none;
+`
---
+++ frontend/src/components/Background/Background.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Image } from './Background-Elements'
+
+const Background: React.FC = () => {
+	return (
+		<Image src='/images/background.jpg'/>
+	)
+}
+
+export default Background
