46b79d9e294d34a6ed9e1dcafd7736080de23754
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-04-25 15:30:59 +0100

    moved background elements into main file

--- frontend/src/components/Background/Background.tsx
+++ frontend/src/components/Background/Background.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
-import { Image } from './Background-Elements'
+export const Image = styled.img`
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+
+	z-index: -1;
+	user-select: none;
+`
 
 const Background: React.FC = () => {
 	return (
