7ba83b2b7a509240fedd664bb909403be6adc64f
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-04-25 10:49:29 +0100

    skills detailed template

--- frontend/src/pages/Skills/Skills-Elements.ts
+++ frontend/src/pages/Skills/Skills-Elements.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { blue, red } from 'utils/colours'
 import corner from 'utils/corner'
 
 export const Title = styled.div`
@@ -7,16 +8,51 @@ export const Title = styled.div`
 	text-transform: uppercase;
 `
 
+export const TreeNamesWrapper = styled.div`
+	grid-area: treenames;
+	display: flex;
+`
+
+export const TreeName = styled.p`
+	padding-right: 20px;
+	font-size: 1.6rem;
+	text-transform: uppercase;
+`
+
 export const Trees = styled.div`
 	grid-area: skills;
+	${corner}
 `
+
+export const SubtreeSkills = styled.div``
 
 export const Reset = styled.div`
 	grid-area: reset;
 `
 
+export const ResetText = styled.p`
+	color: ${blue};
+	text-transform: uppercase;
+	font-size: 1.6rem;
+	direction: rtl;
+`
+
 export const Points = styled.div`
 	grid-area: points;
+	color: ${red};
+	font-size: 1.8rem;
+	display: flex;
+	justify-content: space-between;
+`
+
+export const PointsText = styled.p`
+	color: inherit;
+	text-transform: uppercase;
+`
+
+export const PointsNumber = styled.p`
+	color: inherit;
+	direction: rtl;
 `
 
 export const Info = styled.div`
--- frontend/src/pages/Skills/Skills.tsx
+++ frontend/src/pages/Skills/Skills.tsx
@@ -2,21 +2,39 @@ import Back from 'components/Back'
 import Container from 'components/Container'
 import React from 'react'
 
-import { Info, Points, Reset, Title, Trees } from './Skills-Elements'
+import { Info, Points, PointsNumber, PointsText, Reset, ResetText, SubtreeSkills, Title, TreeName, TreeNamesWrapper, Trees } from './Skills-Elements'
 
 const Skills: React.FC = () => {
 	return (
-		<Container columns='3fr 1fr' rows='1fr 1fr 7fr 1fr' areas='"title reset" "skills points" "skills info" "skills back"'>
+		<Container columns='3fr 1fr' rows='1fr 2rem 7fr 1fr' areas='"title reset" "treenames points" "skills info" "skills back"'>
 			
 			<Title>Skills</Title>
 
-			<Trees>Trees</Trees>
+			<TreeNamesWrapper>
+				<TreeName>Mastermind</TreeName>
+				<TreeName>Enforcer</TreeName>
+				<TreeName>Technician</TreeName>
+				<TreeName>Ghost</TreeName>
+				<TreeName>Fugitive</TreeName>
+			</TreeNamesWrapper>
 
-			<Reset>Reset</Reset>
+			<Trees>
+				<SubtreeSkills></SubtreeSkills>
+				<SubtreeSkills></SubtreeSkills>
+				<SubtreeSkills></SubtreeSkills>
+			</Trees>
 
-			<Points>Points</Points>
+			<Reset>
+				<ResetText>Reset this tree</ResetText>
+				<ResetText>Reset all trees</ResetText>
+			</Reset>
 
-			<Info>Info</Info>
+			<Points>
+				<PointsText>Remaining Skill Points</PointsText>
+				<PointsNumber>0</PointsNumber>
+			</Points>
+
+			<Info></Info>
 
 			<Back />
 
