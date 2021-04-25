44c3e59bc5203a7d2444b29a805ff8d5ad50e6ca
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-04-25 16:14:09 +0100

    removed backend competely

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export default {
-	port: 5000
-}
--- backend/routes/abilities.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import skills from '../data/abilities/skills.js'
-
-const router = express.Router()
-
-router.get('/skills', (req, res) => res.json(skills))
-
-export default router
