14974353bcf49cfb9b40714f70f576517148b4c7
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-04-26 13:06:44 +0100

    subtree labels

--- src/pages/Skills/Tree/Tree-Elements.ts
+++ src/pages/Skills/Tree/Tree-Elements.ts
@@ -1,9 +1,32 @@
 import styled from 'styled-components'
+import { blue } from 'utils/colours'
 import corner from 'utils/corner'
 
 export const Container = styled.div`
 	grid-area: skills;
+	display: flex;
+	justify-content: space-between;
+`
+
+export const Subtree = styled.div`
+	height: 100%;
+	width: calc((100% / 3) - 6px);
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: 1fr 3rem;
+`
+
+export const Label = styled.p`
+	color: ${blue};
+	text-align: center;
+	font-size: 2rem;
+`
+
+export const TierWrapper = styled.div`
 	${corner}
 `
 
-export const SubtreeSkills = styled.div``
+export const Tier = styled.div`
+	height: 25%;
+	width: 100%;
+`
--- src/pages/Skills/Tree/Tree.tsx
+++ src/pages/Skills/Tree/Tree.tsx
@@ -1,15 +1,35 @@
+import data from 'data/abilities/skills'
 import React from 'react'
 
-import { Container } from './Tree-Elements'
+import { Container, Label, Subtree, Tier, TierWrapper } from './Tree-Elements'
 
-interface tree { 
+interface tree {
 	tree: 'mastermind' | 'enforcer' | 'technician' | 'ghost' | 'fugitive'
 }
 
 const Tree: React.FC<tree> = ({tree}: tree) => {
 	return (
 		<Container>
-			{tree}
+			{
+				Object.values(data[tree]).map((values, i) => {
+
+					const label = Object.keys(data[tree])[i]
+
+					return (
+						<Subtree>
+							
+							<TierWrapper>
+								<Tier></Tier>
+								<Tier></Tier>
+								<Tier></Tier>
+								<Tier></Tier>
+							</TierWrapper>
+
+							<Label>{label}</Label>
+						</Subtree>
+					)
+				})
+			}
 		</Container>
 	)
 }
