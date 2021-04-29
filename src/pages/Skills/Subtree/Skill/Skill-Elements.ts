e10d8981a7ecae2f21efc38c6395fd3150c396cb
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-04-29 14:52:59 +0100

    added lock over locked skills

--- src/pages/Skills/Subtree/Skill/Skill-Elements.ts
+++ src/pages/Skills/Subtree/Skill/Skill-Elements.ts
@@ -5,22 +5,41 @@ export const Container = styled.div`
 	width: 50%;
 `
 
+export const Icon = styled.div`
+	padding-bottom: 50%;
+	height: 0;
+	width: 50%;
+	margin-left: 25%;
+	position: relative;
+`
+
+export const Locked = styled.div`
+	background: url("/images/skills/locked.png") 0 0 no-repeat;
+	background-size: 50% 50%;
+	height: 100%;
+	width: 100%;
+	top: 25%;
+	left: 25%;
+	position: absolute;
+`
+
 interface icon {
 	x: number;
 	y: number;
 	state: string
 }
 
-export const Icon = styled.div<icon>`
+export const SkillIcon = styled.div<icon>`
 	background-position-x: calc((${props => props.x}% / 880) * 100) !important;
 	background-position-y: calc((${props => props.y}% / 960) * 100) !important;
 	background: url("/images/skills/skills.png") 0 0 no-repeat;
 	background-size: 1200% 1300%;
-	padding-bottom: 50%;
-	height: 0;
-	width: 50%;
-	margin-left: 25%;
-	opacity: ${props => props.state === 'locked' || props.state === 'available' ? 0.2 : 1};
+	opacity: ${props => props.state === 'locked' || props.state === 'available' ? 0.3 : 1};
+	height: 100%;
+	width: 100%;
+	top: 0;
+	left: 0;
+	position: absolute;
 `
 
 export const Label = styled.p`
--- src/pages/Skills/Subtree/Skill/Skill.tsx
+++ src/pages/Skills/Subtree/Skill/Skill.tsx
@@ -1,7 +1,7 @@
 import { skillData } from 'data/abilities/skills'
 import React, { useState } from 'react'
 
-import { Container, Icon, Label } from './Skill-Elements'
+import { Container, Icon, Label, Locked, SkillIcon } from './Skill-Elements'
 
 interface skillComponent {
 	data: skillData;
@@ -16,7 +16,12 @@ const Skill: React.FC<skillComponent> = ({ data, setSkillHovered }: skillCompone
 
 	return (
 		<Container onMouseOver={() => setSkillHovered(data.name)} onMouseLeave={() => setSkillHovered(undefined)}>
-			<Icon x={data.pos[0]} y={data.pos[1]} state={skillState}/>
+			<Icon>
+				{
+					skillState === 'locked' ? <Locked /> : <></>
+				}
+				<SkillIcon x={data.pos[0]} y={data.pos[1]} state={skillState}/>
+			</Icon>
 			<Label>{data.name}</Label>
 		</Container>
 	)
