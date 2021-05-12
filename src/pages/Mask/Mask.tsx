83176c1c6248c260408fecbde6f9d85cd08a766c
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-05-12 14:45:30 +0100

    added mask selection

--- src/pages/Mask/Mask.tsx
+++ src/pages/Mask/Mask.tsx
@@ -1,13 +1,20 @@
+import { changeMask } from 'actions/characterAction'
 import Back from 'components/Back'
 import Container from 'components/Container'
 import data, { maskData } from 'data/character/masks'
+import { useAppDispatch } from 'hooks'
 import React, { useState } from 'react'
+import { useHistory } from 'react-router'
 
 import { Info, InfoCost, InfoDescription, InfoName, InfoUnlock, Mask, MaskWrapper, Title } from './Mask-Elements'
 
 const MaskPage: React.FC = () => {
 
 	const [hoveredMask, setHoveredMask] = useState<maskData | null>()
+
+	const dispatch = useAppDispatch()
+
+	const history = useHistory()
 
 	return (
 		<Container columns='3fr 1fr' rows='1fr 8fr 4rem' areas='"title title" "masks info" "masks back"'>
@@ -16,12 +23,16 @@ const MaskPage: React.FC = () => {
 
 			<MaskWrapper>
 				{
-					data.map(mask => {
+					data.map((mask, i) => {
 						return <Mask
 							onMouseEnter={() => setHoveredMask(mask)}
 							onMouseLeave={() => setHoveredMask(null)}
+							onMouseDown={() => {
+								dispatch(changeMask(mask))
+								history.push('/')
+							}}
 							src={`images/masks/${mask.imageColour}.png`}
-							key={`${mask.name}`}
+							key={`mask-${mask.name}-${i}`}
 						/>
 					})
 				}
