6b5b08606c86ca54ac65321107660947177cd857
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-05-13 16:48:50 +0100

    localstorage support

--- src/store.ts
+++ src/store.ts
@@ -2,11 +2,24 @@ import { createStore } from "redux";
 
 import allReducers from "./reducers";
 
+const saveToLocalStorage = (state: any) => {
+	const serializedState = JSON.stringify(state)
+	localStorage.setItem('persistantState', serializedState)
+}
+
+const loadFromLocalStorage = () => {
+	const serializedState = localStorage.getItem('persistantState')
+	return serializedState ? JSON.parse(serializedState) : undefined
+}
+
 const global: any = window
 const store = createStore(
 	allReducers,
+	loadFromLocalStorage(),
 	global.__REDUX_DEVTOOLS_EXTENSION__ && global.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export type RootState = ReturnType<typeof store.getState>
 
