9183d610dd4a9d50b994ee9dd7664bdf3a749e42
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-05-13 16:39:46 +0100

    skill selector preview

--- src/pages/Home/Selector/Selector.tsx
+++ src/pages/Home/Selector/Selector.tsx
@@ -1,20 +1,42 @@
+import { useAppSelector } from 'hooks'
 import React from 'react'
+import { grey } from 'utils/colours'
 
-import { Container, Image, Link, Title } from './Selector-Elements'
+import { Container, Image, Link, SkillWrapper, SkillsAmount, SkillsCard, SkillsContainer, Title } from './Selector-Elements'
 
-interface type {
+interface selectorComponent {
 	path: string;
 	title: string;
 	imagePath?: string;
 }
 
-const Selector: React.FC<type> = ({ path, title, imagePath }) => {
+const Selector: React.FC<selectorComponent> = ({ path, title, imagePath }) => {
+
+	const skillTrees = useAppSelector(state => Object.values(state.skills.trees))
 
 	return (
 		<Link to={path}>
 			<Container>
 				<Title>{title}</Title>
-				<Image src={imagePath}/>
+				{
+					imagePath && <Image src={imagePath}/>
+				}
+				{
+					title === 'skills' && <SkillsContainer>
+						{
+							skillTrees.map((tree, i) => {
+								let treePoints = 0
+								for (let subtree in tree) {
+									treePoints += tree[subtree].points
+								}
+								return <SkillWrapper key={`selector-skill-${i}`}>
+									<SkillsCard />
+									<SkillsAmount colour={treePoints ? '#fff' : grey}>{treePoints}</SkillsAmount>
+								</SkillWrapper>
+							})
+						}
+					</SkillsContainer>
+				}
 			</Container>
 		</Link>
 	)
