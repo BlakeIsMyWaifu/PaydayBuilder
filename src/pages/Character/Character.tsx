f3f9a08cfdc0d1c6e2b12f2dc230f194ee632803
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-05-24 17:08:42 +0100

    fixed character grid

--- src/pages/Character/Character.tsx
+++ src/pages/Character/Character.tsx
@@ -17,7 +17,7 @@ const Character: React.FC = () => {
 	const history = useHistory()
 
 	return (
-		<Container columns='3fr 1fr' rows='4rem 8fr 4rem' areas='"title title" "wrapper info" "characters back"'>
+		<Container columns='3fr 1fr' rows='4rem 8fr 4rem' areas='"title title" "wrapper info" "wrapper back"'>
 
 			<Title>Character</Title>
 
