7763626b65018df0f0ac684a86fb9ac49369d0fb
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2021-11-13 01:13:26 +0000

    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,15 @@
 import './App.css'
 
 import Background from 'components/Background'
-import Armour from 'pages/Armour'
-import Character from 'pages/Character'
-import CrewManagement from 'pages/CrewManagement'
-import Equipment from 'pages/Equipment'
-import Home from 'pages/Home'
-import Mask from 'pages/Mask'
-import Melee from 'pages/Melee'
-import PerkDeck from 'pages/PerkDeck'
-import Skills from 'pages/Skills'
-import Throwable from 'pages/Throwable'
-import Weapons from 'pages/Weapons'
 import React from 'react'
-import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import AppRoutes from 'routes/AppRoutes'
 import { isDev } from 'utils/isDev'
 
 const App: React.FC = () => {
 	return (
 		<div onContextMenu={event => isDev() ? null : event.preventDefault()}>
 			<Background />
-			<MemoryRouter>
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/mask' element={<Mask />} />
-					<Route path='/character' element={<Character />} />
-					<Route path='/armour' element={<Armour />} />
-					<Route path='/equipment' element={<Equipment />} />
-
-					<Route path='/primary' element={<Weapons slot='primary' />} />
-					<Route path='/secondary' element={<Weapons slot='secondary' />} />
-					<Route path='/throwable' element={<Throwable />} />
-					<Route path='/melee' element={<Melee />} />
-
-					<Route path='/skills' element={<Skills />} />
-					<Route path='/perkdeck' element={<PerkDeck />} />
-					<Route path='/crewmanagement' element={<CrewManagement />} />
-				</Routes>
-			</MemoryRouter>
+			<AppRoutes />
 		</div>
 	)
 }
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,53 @@
+import React, { Suspense, lazy } from 'react'
+import { Route, MemoryRouter as Router, Routes } from 'react-router-dom'
+
+const Home = lazy(() => import('pages/Home'))
+
+const Mask = lazy(() => import('pages/Mask'))
+const Character = lazy(() => import('pages/Character'))
+const Armour = lazy(() => import('pages/Armour'))
+const Equipment = lazy(() => import('pages/Equipment'))
+
+const Weapons = lazy(() => import('pages/Weapons'))
+const Throwable = lazy(() => import('pages/Throwable'))
+const Melee = lazy(() => import('pages/Melee'))
+
+const Skills = lazy(() => import('pages/Skills'))
+const PerkDeck = lazy(() => import('pages/PerkDeck'))
+const CrewManagement = lazy(() => import('pages/CrewManagement'))
+
+const AppRoutes: React.FC = () => {
+	return (
+		<Router>
+			<Suspense fallback={
+				<div>
+					<p style={{
+						color: 'white'
+					}}>
+						Loading . . .
+					</p>
+				</div>
+			}>
+				<Routes>
+					<Route path='/' element={<Home />} />
+
+					<Route path='/mask' element={<Mask />} />
+					<Route path='/character' element={<Character />} />
+					<Route path='/armour' element={<Armour />} />
+					<Route path='/equipment' element={<Equipment />} />
+
+					<Route path='/primary' element={<Weapons slot='primary' />} />
+					<Route path='/secondary' element={<Weapons slot='secondary' />} />
+					<Route path='/throwable' element={<Throwable />} />
+					<Route path='/melee' element={<Melee />} />
+
+					<Route path='/skills' element={<Skills />} />
+					<Route path='/perkdeck' element={<PerkDeck />} />
+					<Route path='/crewmanagement' element={<CrewManagement />} />
+				</Routes>
+			</Suspense>
+		</Router>
+	)
+}
+
+export default AppRoutes
