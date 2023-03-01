28bd7cb41de23e202d1daa5479322083f8c09532
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2023-03-01 08:57:05 +0000

    🚑 Fix import typo

--- src/hooks/useBuildImporter.ts
+++ src/hooks/useBuildImporter.ts
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { defaultBuild, useBuildsStore } from 'state/useBuildsStore'
-import { stringifyUrlQuery } from 'utils/StringifyUrlQuery'
+import { stringifyUrlQuery } from 'utils/stringifyUrlQuery'
 
 const useBuildImporter = (): boolean => {
 
